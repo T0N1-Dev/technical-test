14bef3f94d6e56fc077649f4ebbb8d26d468a3e9
Antonio Cruz <antoniocruzglez24@gmail.com>
2025-04-27 13:09:33 -0400

    feat(trending): Adding some styles to Trending Categories

--- src/components/TrendingCategories.jsx
+++ src/components/TrendingCategories.jsx
@@ -2,14 +2,14 @@ import { useState } from "react";
 import "../styles/TrendingCategories.css";
 
 const categories = [
-  { id: "01", name: "Action Games", color: "#FF8A65" },
-  { id: "02", name: "Sports Games", color: "#64B5F6" },
-  { id: "03", name: "Adventure Games", color: "#E57373" },
-  { id: "04", name: "Arcade Games", color: "#81C784" },
-  { id: "05", name: "Fantasy Game", color: "#BA68C8" },
-  { id: "06", name: "Strategy Game", color: "#4FC3F7" },
-  { id: "07", name: "Shooter Games", color: "#9575CD" },
-  { id: "view", name: "All Categories", label: "VIEW ALL", color: "#90A4AE" },
+  { id: "01", name: "Action Games", color: "#FF8A65", img: "https://res.cloudinary.com/dmfs1od9n/image/upload/v1745769809/collage-maker-15-aug-2023-09-00-pm-3219_deq6ts.avif" },
+  { id: "02", name: "Sports Games", color: "#64B5F6", img: "https://res.cloudinary.com/dmfs1od9n/image/upload/v1745769892/sport_games_jlidhg.webp" },
+  { id: "03", name: "Adventure Games", color: "#E57373", img: "https://res.cloudinary.com/dmfs1od9n/image/upload/v1745769985/Adventure_Games_yopips.avif" },
+  { id: "04", name: "Arcade Games", color: "#81C784", img: "https://res.cloudinary.com/dmfs1od9n/image/upload/v1745770082/Arcade_Games_ercudc.jpg" },
+  { id: "05", name: "Fantasy Game", color: "#BA68C8", img: "https://res.cloudinary.com/dmfs1od9n/image/upload/v1745770101/Fantasy_games_ckdmeq.avif" },
+  { id: "06", name: "Strategy Game", color: "#4FC3F7", img: "https://res.cloudinary.com/dmfs1od9n/image/upload/v1745770211/Strategy_Games_w0dlep.avif" },
+  { id: "07", name: "Shooter Games", color: "#9575CD", img: "https://res.cloudinary.com/dmfs1od9n/image/upload/v1745770221/shooter-games_r2zyor.webp" },
+  { id: "VIEW ALL", name: "All Categories", label: "All Categories", color: "#90A4AE", img: "https://res.cloudinary.com/dmfs1od9n/image/upload/v1740412660/photo_2025-02-24_10-57-07_f1mrcy.jpg" },
 ]
 
 export default function TrendingCategories() {
@@ -24,15 +24,12 @@ export default function TrendingCategories() {
             key={category.id}
             className={`category-card ${activeCategory === category.id ? "active" : ""}`}
             onClick={() => setActiveCategory(category.id)}
-            style={activeCategory === category.id ? { backgroundColor: category.color, color: "white" } : {}}
+            style={activeCategory === category.id ? {
+              backgroundImage: `linear-gradient(rgba(0,0,0,.1), rgba(0,0,0,1)), url(${category.img})`,
+            } : {}}
           >
             <div className="category-header">
-              <span
-                className="category-id"
-                style={{ color: activeCategory === category.id ? "white" : category.color }}
-              >
-                /{category.id}
-              </span>
+              <span className="category-id"> /{category.id} </span>
             </div>
             <div className="category-content">
               <h3 className="category-name">{category.label || category.name}</h3>
