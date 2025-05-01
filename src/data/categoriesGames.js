272e292ec40f2f7b0371f9774bc337cfdac8b7e6
Antonio Cruz <antoniocruzglez24@gmail.com>
2025-05-01 12:37:41 -0400

    fix(data): Fixing image loading delay

--- src/data/categoriesGames.js
+++ src/data/categoriesGames.js
@@ -1,10 +1,10 @@
 export const categoriesGames = [
-  { id: "01", name: "Action Games", color: "#FF8A65", img: "https://res.cloudinary.com/dmfs1od9n/image/upload/v1745769809/collage-maker-15-aug-2023-09-00-pm-3219_deq6ts.avif" },
-  { id: "02", name: "Sports Games", color: "#64B5F6", img: "https://res.cloudinary.com/dmfs1od9n/image/upload/v1745769892/sport_games_jlidhg.webp" },
-  { id: "03", name: "Adventure Games", color: "#E57373", img: "https://res.cloudinary.com/dmfs1od9n/image/upload/v1745769985/Adventure_Games_yopips.avif" },
-  { id: "04", name: "Arcade Games", color: "#81C784", img: "https://res.cloudinary.com/dmfs1od9n/image/upload/v1745770082/Arcade_Games_ercudc.jpg" },
-  { id: "05", name: "Fantasy Game", color: "#BA68C8", img: "https://res.cloudinary.com/dmfs1od9n/image/upload/v1745770101/Fantasy_games_ckdmeq.avif" },
-  { id: "06", name: "Strategy Game", color: "#4FC3F7", img: "https://res.cloudinary.com/dmfs1od9n/image/upload/v1745770211/Strategy_Games_w0dlep.avif" },
-  { id: "07", name: "Shooter Games", color: "#9575CD", img: "https://res.cloudinary.com/dmfs1od9n/image/upload/v1745770221/shooter-games_r2zyor.webp" },
-  { id: "VIEW ALL", name: "All Categories", label: "All Categories", color: "#90A4AE", img: "https://res.cloudinary.com/dmfs1od9n/image/upload/v1740412660/photo_2025-02-24_10-57-07_f1mrcy.jpg" },
+  { id: "01", name: "Action Games", color: "#FF8A65", img: "https://res.cloudinary.com/dmfs1od9n/image/upload/f_webp,q_auto/v1745769809/collage-maker-15-aug-2023-09-00-pm-3219_deq6ts.avif" },
+  { id: "02", name: "Sports Games", color: "#64B5F6", img: "https://res.cloudinary.com/dmfs1od9n/image/upload/f_webp,q_auto/v1745769892/sport_games_jlidhg.webp" },
+  { id: "03", name: "Adventure Games", color: "#E57373", img: "https://res.cloudinary.com/dmfs1od9n/image/upload/f_webp,q_auto/v1745769985/Adventure_Games_yopips.avif" },
+  { id: "04", name: "Arcade Games", color: "#81C784", img: "https://res.cloudinary.com/dmfs1od9n/image/upload/f_webp,q_auto/v1745770082/Arcade_Games_ercudc.jpg" },
+  { id: "05", name: "Fantasy Game", color: "#BA68C8", img: "https://res.cloudinary.com/dmfs1od9n/image/upload/f_webp,q_auto/v1745770101/Fantasy_games_ckdmeq.avif" },
+  { id: "06", name: "Strategy Game", color: "#4FC3F7", img: "https://res.cloudinary.com/dmfs1od9n/image/upload/f_webp,q_auto/v1745770211/Strategy_Games_w0dlep.avif" },
+  { id: "07", name: "Shooter Games", color: "#9575CD", img: "https://res.cloudinary.com/dmfs1od9n/image/upload/f_webp,q_auto/v1745770221/shooter-games_r2zyor.webp" },
+  { id: "VIEW ALL", name: "All Categories", label: "All Categories", color: "#90A4AE", img: "https://res.cloudinary.com/dmfs1od9n/image/upload/f_webp,q_auto/v1740412660/photo_2025-02-24_10-57-07_f1mrcy.jpg" },
 ]
